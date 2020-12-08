7348e3ad7a9969de581f709639f08e4617b708ee
KoredeDavid <koredeoluwashola@gmail.com>
2020-12-08 11:39:28 +0100

    Removed EMAIL SSL settings

--- edutech/settings/prod.py
+++ edutech/settings/prod.py
@@ -31,7 +31,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+# EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', "")
