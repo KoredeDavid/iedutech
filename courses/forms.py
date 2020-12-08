c4c864cd9f73acba2b52534cf839f7b5b8924e87
KoredeDavid <koredeoluwashola@gmail.com>
2020-12-08 11:29:33 +0100

    Updated index.html, forms.py and course.html

--- courses/forms.py
+++ courses/forms.py
@@ -5,9 +5,9 @@ from .models import NewsLetter
 
 class NewsLetterForm(forms.ModelForm):
     name = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control  ', }))
+        widget=forms.TextInput(attrs={'class': 'form-control  ', 'placeholder': 'Name'}))
     email = forms.CharField(
-        widget=forms.EmailInput(attrs={ 'class': 'form-control  ', }))
+        widget=forms.EmailInput(attrs={ 'class': 'form-control  ', 'placeholder': 'Email' }))
 
 
     class Meta:
