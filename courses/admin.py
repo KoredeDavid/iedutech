884862cbec687bb2edaadb50c8a49526990f1cb4
KoredeDavid <koredeoluwashola@gmail.com>
2020-12-08 17:14:03 +0100

    Added model- NewsLetter to the admin app

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Category, Course
+from .models import CustomUser, Category, Course, NewsLetter
+
 # Register your models here.
 
 admin.site.register(CustomUser)
 admin.site.register(Category)
 admin.site.register(Course)
+admin.site.register(NewsLetter)
