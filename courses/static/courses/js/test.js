116cec066309a30c5beea07db925f174c71b809b
KoredeDavid <koredeoluwashola@gmail.com>
2021-10-09 19:58:04 +0100

    Solve Conflict

--- courses/static/courses/js/test.js
+++ courses/static/courses/js/test.js
@@ -33,11 +33,29 @@ document.addEventListener("DOMContentLoaded", function(){
 
     var el_autohide = document.querySelector('.autohide');
     var nav = document.querySelector('nav')
+    var nav_items = document.querySelector("#navbarSupportedContent")
     var bar = document.querySelector('.fa-bars')
 
 
     if(el_autohide){
         var last_scroll_top = 0;
+        let scroll_top = window.scrollY;
+
+        if (scroll_top !== 0) {
+            el_autohide.style.backgroundColor = 'white'
+        }
+
+        bar.addEventListener('click', function () {
+            if (nav_items.classList.contains('show')){
+                nav.classList.remove('collapse-bg')
+                console.log('removed')
+            }
+            else{
+                nav.classList.add('collapse-bg')
+                console.log('add')
+            }
+        })
+
         window.addEventListener('scroll', function() {
             let scroll_top = window.scrollY;
 
@@ -64,11 +82,8 @@ document.addEventListener("DOMContentLoaded", function(){
             // console.log(last_scroll_top, scroll_top)
 
             last_scroll_top = scroll_top;
-
-
         });
-        bar.addEventListener('click', function () {
-            nav.classList.toggle('collapse-bg')
-        })
     }
 });
+
+// nav.classList.toggle('collapse-bg')
--- edutech/urls.py
+++ edutech/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('sayches', include('sayches.urls')),
     path('', include('courses.urls'))
 ]
