c698dd50d78eaa316cb03e154864d1976b151e9d
KoredeDavid <koredeoluwashola@gmail.com>
2023-01-04 16:05:02 +0100

    Edit prod settings

--- edutech/settings/prod.py
+++ edutech/settings/prod.py
@@ -95,7 +95,8 @@ ADMINS = [
 
 MANAGERS = ADMINS
 
-import dj_database_url
+if not sqlite:
+    import dj_database_url
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+    prod_db = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(prod_db)
